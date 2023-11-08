dacd4e4aa822bdfa801e19043e94299c63194d3d
2000Nathan <Natan747mr@gmail.com>
2023-11-08 17:26:44 -0600

    Config redis

--- src/utils/users/index.ts
+++ src/utils/users/index.ts
@@ -1,4 +1,5 @@
 import { Connection }  from "mysql2";
+import redis from "../../services/redis";
 
 export class UserUtils {
     private database;
@@ -7,12 +8,23 @@ export class UserUtils {
         this.database = db;
     }
 
-    async function getUser(){
+    async getUser() {
+        const existCahce = await redis.get('allUsers');
+        if (existCahce) {
+            return JSON.parse(existCahce);
+        } 
         const [rows, fields] = await this.database.query('SELECT * FROM users');
-        return {
+        const sendable = {
             rows,
             fields
         }
+        await redis.set('allUsers', JSON.stringify(sendable));
+
+        const todayEnd = new Date().setHours(23, 59, 59, 999);
+
+        redis.expireAt("allUsers", todayEnd/1000);
+
+        return sendable;
         
     }
 }
